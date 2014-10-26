2263237e063e57aefd39f17521273d60ba7d0e78
Mihai <Mihai@Mihai.Dlink>
2014-10-26 11:59:43 +0200

    remake project

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,9 @@
-
+if ($(".slider-home").length > 0) {
+    $('.slider-home').bxSlider({
+        mode: 'vertical',
+        auto: true
+    });
+    $("#openBlock").click(function () {
+        $(this).parents(".animated").toggleClass("open");
+    });
+}
